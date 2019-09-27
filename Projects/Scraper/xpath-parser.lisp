cfda655a31bf9381020d604da92fa6713d4f70c0
sharklasers996 <sharklasers996@gmail.com>
2019-09-27 08:19:33 +0300

    Added xpath parsing funcs

--- Projects/Scraper/xpath-parser.lisp
+++ Projects/Scraper/xpath-parser.lisp
@@ -1,4 +1,4 @@
-(defparameter *test-xpath* "//table//div/a/img")
+(defparameter *test-xpath* "//table[@class='desraite/desra/']//div[@id='/opa/opa']/a/img")
 
 (defun count-occurrences (search-string input)
   "Counts search-string occurrences in input"
@@ -23,6 +23,7 @@
     indices))
 
 (defun find-index-pairs (search-string input)
+  "Same as find-indices, but returns indices as a list of pairs"
   (let* ((result (list))
         (indices (find-indices search-string input)))
     (labels ((take-two (lst)
@@ -33,3 +34,38 @@
         (error "Index count is not even"))
       (take-two indices))
     (reverse result)))
+
+
+(defun remove-intersecting-indices (indices index-pairs)
+  "Removes from indices where item falls between an index-pair"
+  (dolist (i index-pairs)
+    (setf indices (remove-if #'(lambda(x) (and (< (first i) x) (> (second i) x))) indices)))
+  indices)
+
+
+(defun remove-neighboring-indices (indices)
+  "Removes numbers from list which have a (1- i) member
+  (0 2 3 5 7 8) => (0 2 5 7)"
+  (let ((result-indices (list)))
+        (dolist (i indices)
+          (when (not (member (1- i) result-indices))
+            (push i result-indices)))
+    (reverse result-indices)))
+
+
+(defun split-xpath (xpath)
+  "Splits xpath into a list of single element tokens"
+  (let* ((slash-indices (find-indices "/" xpath))
+        (quote-index-pairs (find-index-pairs "'" xpath))
+        (not-ignored-slash-indices (remove-intersecting-indices slash-indices quote-index-pairs))
+        (non-neighboring-indices (remove-neighboring-indices not-ignored-slash-indices))
+        (current-index nil)
+        (next-index nil))
+    (dotimes (i (length non-neighboring-indices))
+      (setf current-index (pop non-neighboring-indices))
+      (when (< 0 (length non-neighboring-indices))
+        (setf next-index (first non-neighboring-indices)))
+      (if next-index
+          (print (subseq xpath current-index next-index))
+          (print (subseq xpath current-index)))
+      (setf next-index nil))))
